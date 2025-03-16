dd72f122ff9095937d5585ee80ecff3783ed4c52
jeonhyeonseon <good_pretty@naver.com>
2025-03-16 20:52:26 +0900

    chore: 초기 설정

---
+++ dietary-management/src/main/java/com/main/dietary_management/DietaryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.main.dietary_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietaryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietaryManagementApplication.class, args);
+	}
+
+}
---
+++ dietary-management/src/test/java/com/main/dietary_management/DietaryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.dietary_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietaryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
